3d57c8a7203d2a2759d65e419ba3ddde05e81116
CherryC9H13N <836337920@qq.com>
2026-01-22 20:16:49 +0800

    test

--- packages/deck-builder/src/Card.tsx
+++ packages/deck-builder/src/Card.tsx
@@ -68,7 +68,6 @@ export function Card(props: CardProps) {
       </div>
       <div
         class="absolute inset-0 bg-transparent @3xl:hidden"
-        onContextMenu={(e) => e.preventDefault()}
         data-long-press-delay="500"
         on:long-press={(e: CustomEvent) => {
           showCard(e, props.type, props.id);
