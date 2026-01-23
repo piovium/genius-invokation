bfdfac4d851062452a20e138d7817d9369ce8e1c
Guyutongxue <guyutongxue@163.com>
2026-01-23 09:59:14 +0800

    revert UT
    Change-Id: Ica1927f330ce175d3b2b7ab5888761d76ecd44f1

--- packages/test/__tests__/la_signora.test.tsx
+++ packages/test/__tests__/la_signora.test.tsx
@@ -13,18 +13,23 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { Card, Character, CombatStatus, ref, setup, State, Status } from "#test";
+import { Card, Character, DeclaredEnd, ref, setup, State, Status } from "#test";
 import { TeyvatFriedEgg } from "@gi-tcg/data/internal/cards/event/food";
 import {
   IcesealedCrimsonWitchOfEmbers,
   LaSignora,
 } from "@gi-tcg/data/internal/characters/cryo/la_signora";
-import { GrassRingOfSanctification, KukiShinobu } from "@gi-tcg/data/internal/characters/electro/kuki_shinobu";
-import { BestialAscent, Gaming } from "@gi-tcg/data/internal/characters/pyro/gaming";
+import {
+  Chevreuse,
+  RingOfBurstingGrenades,
+} from "@gi-tcg/data/internal/characters/pyro/chevreuse";
+import { Aura } from "@gi-tcg/typings";
+import { expect } from "bun:test";
 import { test } from "bun:test";
 
 test("la signora: death in cryo state", async () => {
   const laSignora = ref();
+  const otherOpp = ref();
   const c = setup(
     <State>
       <Card opp def={TeyvatFriedEgg} />
@@ -34,18 +39,19 @@ test("la signora: death in cryo state", async () => {
         def={LaSignora}
         ref={laSignora}
         health={1}
+        aura={Aura.Electro}
       >
         <Status def={IcesealedCrimsonWitchOfEmbers} />
       </Character>
-      <Character opp />
-      <Character my def={Gaming} />
-      <CombatStatus my def={GrassRingOfSanctification} />
-      <Character my def={KukiShinobu} />
+      <Character opp aura={Aura.Electro} ref={otherOpp} />
+      <Character opp alive={0} />
+      <Character my active def={Chevreuse} energy={2} />
     </State>,
   );
-  // 嘉明E技能内火伤+切人，打死女士，女士免于被击倒回1
-  // 切人触发鸣草结环再打1，以冰形态击倒
-  await c.me.skill(BestialAscent);
+  // 火伤打雷底，触发超载，女士免于被击倒
+  // 超载触发“二重毁伤弹”火伤，再触发超载，再切回女士
+  // 再触发一次火伤，将女士以冰形态击倒
+  await c.me.skill(RingOfBurstingGrenades);
 
   // 死了
   c.expect(laSignora).toHaveVariable({ alive: 0 });
@@ -56,6 +62,7 @@ test("la signora: death in cryo state", async () => {
   // 仍然是冰形态
   c.expect(`with id ${laSignora.id}`).toBeDefinition(LaSignora);
 
+  await c.opp.chooseActive(otherOpp);
   await c.opp.card(TeyvatFriedEgg, laSignora);
 
   c.expect(laSignora).toHaveVariable({ alive: 1, health: 1 });
