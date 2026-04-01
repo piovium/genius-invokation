bbbced60b8d5528ed5cbb2b55559f4bb3260da62
Guyutongxue <guyutongxue@163.com>
2026-04-01 18:14:51 +0800

    Revert "fix(standalone): add editor back button (#684)"
    This reverts commit 17e8ff9ecde74d4a327a166fe56e06965b7ee815.

--- packages/standalone/src/App.tsx
+++ packages/standalone/src/App.tsx
@@ -323,19 +323,10 @@ export function App() {
           />
         </Match>
         <Match when={mode() === GameMode.Editor}>
-          <>
-            <button
-              class="editor-back-button"
-              type="button"
-              onClick={() => setMode(GameMode.NotStarted)}
-            >
-              返回首页
-            </button>
-            <GameStateEditor
-              initialValue={editorInitialState()}
-              onSubmit={onEditorSubmit}
-            />
-          </>
+          <GameStateEditor
+            initialValue={editorInitialState()}
+            onSubmit={onEditorSubmit}
+          />
         </Match>
       </Switch>
       <dialog ref={deckBuilderDialog!} class="deck-builder-dialog">
