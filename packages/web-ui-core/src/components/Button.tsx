4fc4cbc2d63a5c49314ac4229468cb64b562b0fc
CherryC9H13N <836337920@qq.com>
2026-04-25 18:25:04 +0800

    refactor Button css

--- packages/web-ui-core/src/components/Button.tsx
+++ packages/web-ui-core/src/components/Button.tsx
@@ -29,17 +29,15 @@ export function Button(props: ButtonProps) {
   const ch = children(() => props.children);
   return (
     <button
-      class={`grid h-10.8 w-45 group/confirm_btn bg-transparent ${
+      class={`grid group/confirm_btn bg-transparent ${
         props.class ?? ""
       }`}
       onClick={(e) => props.onClick(e)}
     >
+      <ButtonNormal class="grid-area-[1/1] w-45 h-10.8 block group-[:is(:hover,:active)]/confirm_btn:hidden" />  
+      <ButtonHover class="grid-area-[1/1] w-45 h-10.8 hidden group-[:hover:not(:active)]/confirm_btn:block" />          
       <ButtonActive class="grid-area-[1/1] w-45 h-10.8 hidden group-active/confirm_btn:block" />
-      <ButtonHover class="grid-area-[1/1] w-45 h-10.8 hidden group-[:hover:not(:active)]/confirm_btn:block" />
-      <ButtonNormal class="grid-area-[1/1] w-45 h-10.8 block group-[:is(:hover,:active)]/confirm_btn:hidden" />
-      <div class="grid-area-[1/1] h-10.8 w-45 flex items-center justify-center text-lg font-bold text-black/70 transition-colors line-height-none px-6 box-border">
-        <AutoResizeText class="text-center">{ch()}</AutoResizeText>
-      </div>
+      <AutoResizeText class="grid-area-[1/1] w-30 text-center font-bold text-black/70 self-center mx-auto">{ch()}</AutoResizeText>
     </button>
   );
 }
