79b3fcbb5cf98316583a30414819cfe9f7319dba
CherryC9H13N <836337920@qq.com>
2026-06-03 10:06:36 +0800

    debug

--- packages/web-client/src/App.tsx
+++ packages/web-client/src/App.tsx
@@ -33,6 +33,7 @@ const User = lazy(() => import("./pages/User"));
 const Decks = lazy(() => import("./pages/Decks"));
 const EditDeck = lazy(() => import("./pages/EditDeck"));
 const Room = lazy(() => import("./pages/Room"));
+const FontDebug = lazy(() => import("./pages/FontDebug"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 export interface VersionContextValue {
@@ -88,6 +89,7 @@ function App() {
           <Route path="/decks/:id" component={EditDeck} />
           <Route path="/decks" component={Decks} />
           <Route path="/rooms/:code" component={Room} />
+          <Route path="/debug" component={FontDebug} />
           <Route path="*" component={NotFound} />
         </Router>
       </MobileContext.Provider>
