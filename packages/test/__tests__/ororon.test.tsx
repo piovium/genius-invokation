672dbe57100e9da0c79dd4f62891de98efdd3835
nana-nahida <172745284+nana-nahida@users.noreply.github.com>
2026-07-26 01:29:06 +0800

    chore(test): 修改签名为 Piovium Labs

--- packages/test/__tests__/ororon.test.tsx
+++ packages/test/__tests__/ororon.test.tsx
@@ -1,4 +1,4 @@
-// Copyright (C) 2026 Guyutongxue
+// Copyright (C) 2026 Piovium Labs
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
